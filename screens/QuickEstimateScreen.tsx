e939e5a95a248bf06808d689f2efd007dc683405
Seungbin Oh <sboh1214@gmail.com>
2020-04-13 18:16:47 +0900

    add back feature on webview

--- screens/QuickEstimateScreen.tsx
+++ screens/QuickEstimateScreen.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
-import {Container, Header, Content, Text} from 'native-base';
+import {
+  Container,
+  Header,
+  Content,
+  Text,
+  Body,
+  Input,
+  Form,
+  Item,
+} from 'native-base';
 
 export default function QuickEstimateScreen() {
   return (
     <Container>
-      <Header />
+      <Header>
+        <Body>
+          <Text>빠른 견적</Text>
+        </Body>
+      </Header>
       <Content>
-        <Text>빠른 견적</Text>
+        <Form>
+          <Item>
+            <Input placeholder="Username" />
+          </Item>
+          <Item last>
+            <Input placeholder="Password" />
+          </Item>
+        </Form>
       </Content>
     </Container>
   );
--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,13 +1,63 @@
-import * as React from 'react';
-import {Container, Header, Content} from 'native-base';
+import React, {useState, useEffect} from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Left,
+  Button,
+  Icon,
+  Body,
+  Text,
+  Right,
+} from 'native-base';
 import {WebView} from 'react-native-webview';
 
+type WebViewState = {
+  url?: string;
+  title?: string;
+  loading?: boolean;
+  canGoBack?: boolean;
+  canGoForward?: boolean;
+};
+
 export default function SearchScreen(): JSX.Element {
+  const [webViewState, setWebViewState] = useState<WebViewState>();
+  let webView: any = null;
+  const [headerTitle, setHeaderTitle] = useState('검색');
+
+  const handleWebView = (state: WebViewState) => {
+    setWebViewState(state);
+  };
+
+  useEffect(() => {
+    setHeaderTitle(webViewState?.title ?? '검색');
+  }, [webViewState]);
+
+  const handleBack = () => {
+    if (webViewState?.canGoBack === true) {
+      webView.goBack();
+    }
+  };
+
   return (
     <Container>
-      <Header />
+      <Header>
+        <Left>
+          <Button transparent onPress={handleBack}>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Text>{headerTitle}</Text>
+        </Body>
+        <Right />
+      </Header>
       <Content contentContainerStyle={{flex: 1}}>
-        <WebView source={{uri: 'http://m.danawa.com'}} />
+        <WebView
+          source={{uri: 'http://m.danawa.com'}}
+          onNavigationStateChange={handleWebView}
+          ref={(ref) => (webView = ref)}
+        />
       </Content>
     </Container>
   );
